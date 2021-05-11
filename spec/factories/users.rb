de7ea4609a54273d022eca63a0a36ca6b4611953
Breno Vergilio <bjvergilio47@gmail.com>
2021-05-11 09:13:30 -0400

    followers and following

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do 
     factory :user do
-        name { "Breno" }
-        email { "breno@teste.com" }
+        name { "User" }
+        email { "user#{rand(1..1000)}@teste.com" }
         phone { "88888888" }
         avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'test-avatar.png'), 'image/png') }
         password { "123456" }
