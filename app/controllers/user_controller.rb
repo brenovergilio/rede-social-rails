e9541f552bf325ac3a45f4dce353dd5d65a11ca0
Breno Vergilio <bjvergilio47@gmail.com>
2021-05-10 21:28:08 -0400

    cadastro de usuario

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+    before_action :authenticate_user
+
+    def authenticate_user
+        redirect_to new_user_session_path unless current_user?
+    end
+
+    def current_user?
+        current_user.present? ? true : false
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
   namespace :admin do
     root to: 'home#index'
     resources :admins
+    resources :users, only: :index
+  end
+
+  root to: 'user/timeline#index'
+
+  namespace :user do
+
   end
 end
